42c6aea86d6ee5073c1d020da2df86bf42100758
MaciejMM <maciej.myslinski88@gmail.com>
2024-01-28 16:05:54 +0100

    wip-tailwind implementation

--- src/features/about-page/AboutPictures.tsx
+++ src/features/about-page/AboutPictures.tsx
@@ -1,39 +1,23 @@
 import React from 'react';
 
 export const AboutPictures = () => {
+  const imagePathList = [
+    'images/IMG_20180802_111201.jpg',
+    'images/IMG_20191030_112518.jpg',
+    'images/IMG_20180608_132959.jpg',
+    'images/IMG_20210318_115316.jpg',
+    'images/IMG_20201204_120559.jpg',
+    'images/IMG_20200213_131455.jpg',
+  ];
+
   return (
     <div className="bg-hm-lightgrey py-16 lg:py-20">
       <div className="m-auto grid max-w-c-xl grid-cols-2 gap-4 px-4 lg:gap-8">
-        <img
-          className="aspect-square h-full w-full rounded-md object-cover"
-          src={'images/IMG_20180802_111201.jpg'}
-          alt=""
-        />
-        <img
-          className="aspect-square h-full w-full rounded-md object-cover"
-          src={'images/IMG_20191030_112518.jpg'}
-          alt=""
-        />
-        <img
-          className="aspect-square h-full w-full rounded-md object-cover"
-          src={'images/IMG_20180608_132959.jpg'}
-          alt=""
-        />
-        <img
-          className="aspect-square h-full w-full rounded-md object-cover"
-          src={'images/IMG_20210318_115316.jpg'}
-          alt=""
-        />
-        <img
-          className="aspect-square h-full w-full rounded-md object-cover"
-          src={'images/IMG_20201204_120559.jpg'}
-          alt=""
-        />
-        <img
-          className="aspect-square h-full w-full rounded-md object-cover"
-          src={'images/IMG_20200213_131455.jpg'}
-          alt=""
-        />
+        {imagePathList.map((imagePath: string, index: number) => {
+          return (
+            <img key={index} className="aspect-square h-full w-full rounded-md object-center" src={imagePath} alt="" />
+          );
+        })}
       </div>
     </div>
   );
--- src/features/home-page/HomeMain.tsx
+++ src/features/home-page/HomeMain.tsx
@@ -4,21 +4,19 @@ import { LinkButton } from '../../components/Button';
 
 const HemetH1 = styled.h1`
   color: white;
-  font-size: 4rem;
   margin-bottom: 3rem;
   position: relative;
-  padding-left: 1.2rem;
   padding-top: 4rem;
 
   &::after {
     position: absolute;
     content: '';
     bottom: -6px;
-    //transform: translateY(-50%);
+    border-radius: 1px;
     left: 0;
-    width: 10px;
+    width: 300px;
     background-color: #fed50a;
-    height: 0.75em;
+    height: 5px;
   }
 `;
 
@@ -27,12 +25,12 @@ export const HomeMain = () => {
     <div className="home-main">
       <img src={'images/DJI_0430.jpg'} alt=""></img>
       <div className="m-auto flex h-full max-w-c-xl flex-col justify-center px-4">
-        <h3 className="text-xl font-medium uppercase text-hm-black10">Konstrukcje stalowe</h3>
-        <HemetH1 className="md:text-2xl">HEMET</HemetH1>
-        <div className="description mb-4 font-medium uppercase text-hm-black10">
+        <p className="text-3xl font-medium uppercase text-hm-black10">Konstrukcje stalowe</p>
+        <HemetH1 className="text-9xl">HEMET</HemetH1>
+        <div className="description mb-20 text-xl font-medium uppercase text-hm-black10 lg:text-3xl">
           20 lat doświadczenia w budowaniu <span className="text-hm-yellow">konstrukcji stalowych</span>
         </div>
-        <div className=" flex w-full flex-col gap-8 md:flex-row ">
+        <div className=" flex w-full flex-col gap-8 md:flex-row">
           <LinkButton title={'Nasze projekty'} pageLink={'/projects'}></LinkButton>
           <LinkButton title={'Kontakt'} pageLink={'/contacts'}></LinkButton>
         </div>
