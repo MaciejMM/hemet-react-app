8e3949a358216a80c20865602ca2d582a8cc5e82
rw-MaciejMyslinski <maciej.myslinski@rockwool.com>
2024-02-28 18:47:28 +0100

    improve seo

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,32 @@ import { Footer } from './components/Footer';
 import { ContactPage } from './features/contact/ContactPage';
 import { ProjectPage } from './features/projects/ProjectPage';
 import { useAppStore } from './state/AppState';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   const { showMenu } = useAppStore((state) => ({
     showMenu: state.showMenu,
   }));
 
-  const pages = (<div>
-    <Header />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/contacts" element={<ContactPage />} />
-      <Route path="/projects" element={<ProjectPage />} />
-      <Route path="/about" element={<AboutPage />} />
-    </Routes>
-    <Footer />
-  </div>);
+  const pages = (
+    <div>
+      <Helmet>
+        <title>Hemet - Konstrukcje stalowe</title>
+        <meta name="description" content="20 lat doświadczenia w budowaniu konstrukcji stalowych" />
+        <meta
+          name="keywords"
+          content="hale, hale produkcyjne, hale obsługowe, obiekty stalowe, obiekty magazynowe, obiekty użyteczności publicznej, konstrukcje stalowe"
+        />
+      </Helmet>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contacts" element={<ContactPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
+    </div>);
 
   if (showMenu) {
     return (<MobileMenu></MobileMenu>);
